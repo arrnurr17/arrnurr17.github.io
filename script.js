f727fa1e59b24ab3f4b7c74ad0ead90e6c02f8b1
arrnurr17 <bulatovarnur@gmail.com>
2025-02-07 16:23:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll("nav ul li a");
 
     navLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            navLinks.forEach(link => link.classList.remove("active"));
-            link.classList.add("active");
-        });
-    });
-});
+        link.add
